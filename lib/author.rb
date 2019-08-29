fc5d0d00020172ab73c981b8796017abca986682
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-29 18:15:48 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,11 +12,17 @@ class Author
   def posts 
      Post.all.select{|post| post.author == self}
   end
+  
   def add_post(post)
     @posts << post
     post.author = self 
   end
+  
   def self.all 
     @@all 
+  
+  
+  def add_post_by_title(post)
+    
   end
 end
