a3f1814a100d826f9fa3fcec4e6db667220a2403
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-29 19:16:53 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,7 +15,7 @@ class Artist
   def add_song(song)
     # @songs << song 
      song.artist = self
-     @@song_count 
+     @@song_count += 1  
   end
   
   
@@ -30,6 +30,7 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count += 1 
   end
   
   def self.song_count
