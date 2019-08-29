af54beb34a0e3b5407ff76f47724305e8ec583bb
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-29 18:21:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -14,14 +14,22 @@ class Artist
     @songs << song 
     song.name = self
   end
+  
   def add_song_by_name(name)
     
   end
+  
   def songs
     Song.all.select{|s| s.artist == self}
   end
+  
   def self.all 
     @@all 
   end 
+  
+  def add_song_by_name(song)
+    song = Song.new(song)
+  end
+  
   
 end
